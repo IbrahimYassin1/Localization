a79be7a34c3f551e9f8c9fcd945957acee701618
IbrahimYassin1 <ibbe_nba@hotmail.com>
2020-09-16 10:53:57 +0200

    added greeting

--- app/src/main/java/com/example/localization/MainActivity.kt
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -2,16 +2,39 @@ package com.example.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingTextView: TextView
+    var name = "Ibbe"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingTextView = findViewById(R.id.textView)
     }
+
+
+
+    fun button_pressed(view:View){
+        greetingTextView.text = getString(R.string.great)
+    }
+
+    // eng, Good job Ibbe!!
+    //sve, Bra jobbat Ibbe!!
+
+
 }
 
 
+
 // Anpassa appen till svensak och engelska
-// 1. Anpassa xml - Layout//
-// 2. Anpassa kotlin-fil
+// 1. Anpassa xml - Layout X
+// 2. Anpassa kotlin-fil  X
 // 3. Anpassa bilder
